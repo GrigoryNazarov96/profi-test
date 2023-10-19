37043243ff218642703a9287dcd3055916a361a1
Gregory <gnazarov16@gmail.com>
2023-10-19 07:10:58 -0300

    copy to clipboard functionality added

--- public/index.js
+++ public/index.js
@@ -1,10 +1,11 @@
-const button = document.getElementById("generateButton");
-const inputLink = document.getElementById("inputLink");
-const inputCustom = document.getElementById("inputCustom");
+const generateButton = document.getElementById("generateButton");
+const copyButton = document.getElementById("copyButton");
 
-if (button) {
-  button.addEventListener("click", (e) => {
+if (generateButton) {
+  generateButton.addEventListener("click", (e) => {
     e.preventDefault();
+    const inputLink = document.getElementById("inputLink");
+    const inputCustom = document.getElementById("inputCustom");
     const link = inputLink.value;
     const suffix = inputCustom.value;
     if (suffix) {
@@ -12,6 +13,14 @@ if (button) {
     } else {
       generateShortUrl(link);
     }
+  });
+}
+
+if (copyButton) {
+  copyButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const shortenedLink = document.getElementById("shortenedLink");
+    copyToClipboard(shortenedLink.href);
   });
 }
 
@@ -41,17 +50,29 @@ function displayShortenedLink(link) {
 }
 
 function displayError(e) {
-  const errorContainer = document.getElementById("errorContainer");
-  errorContainer.textContent = e;
-  errorContainer.style.display = "block";
+  const messageContainer = document.getElementById("messageContainer");
+  messageContainer.textContent = e;
+  messageContainer.style.display = "block";
 }
 
 function hideError() {
-  const errorContainer = document.getElementById("errorContainer");
-  errorContainer.style.display = "none";
+  const messageContainer = document.getElementById("messageContainer");
+  messageContainer.style.display = "none";
 }
 
 function hideShortenedLink() {
   const linkContainer = document.getElementById("linkContainer");
   linkContainer.style.display = "none";
+}
+
+function copyToClipboard(link) {
+  navigator.clipboard.writeText(link).then(() => callbackCopyToClipboard("Link copied to Clipboard"));
+}
+
+function callbackCopyToClipboard(text) {
+  const messageContainer = document.getElementById("messageContainer");
+
+  messageContainer.style.display = "block";
+  messageContainer.style.color = "green";
+  messageContainer.textContent = text;
 }
